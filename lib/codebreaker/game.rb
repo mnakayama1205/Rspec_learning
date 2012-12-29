e5972e16f4daff1cc7daab97fd131080601b3bb1
Makoto Nakayama <makoto0306@gmail.com>
2012-12-29 16:11:09 +0900

    393

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -11,23 +11,30 @@ module Codebreaker
     end
 
     def guess(guess)
-      exact_match_count = 0
+      
       number_match_count = 0
-      (0..3).each do |index|
-        if exact_match?(guess, index)
-          exact_match_count += 1
-        end
-      end
+      
       (0..3).each do |index|
         if number_match?(guess, index)
           number_match_count += 1
         end
       end
-      @output.puts '+'*exact_match_count + '-'*number_match_count
+      @output.puts '+'*exact_match_count(guess) + '-'*number_match_count
+    end
+    
+    def exact_match_count(guess)
+      exact_match_count = 0
+      (0..3).each do |index|
+        if exact_match?(guess, index)
+          exact_match_count += 1
+        end
+      end
+      exact_match_count
     end
 
     def exact_match?(guess, index)
       guess[index] == @secret[index]
+      
     end
 
     def number_match?(guess, index)
